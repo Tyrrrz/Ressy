50a835cce82a16e48d882b3662ac200eadca4cb0
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2021-10-31 01:42:06 +0300

    Fix file concurrency issues in tests

--- Ressy.Tests/Fixtures/DummyFixture.cs
+++ Ressy.Tests/Fixtures/DummyFixture.cs
@@ -1,24 +1,31 @@
 ﻿using System;
 using System.IO;
+using System.Threading;
 using Ressy.Tests.Utils;
 
 namespace Ressy.Tests.Fixtures
 {
     public class DummyFixture : IDisposable
     {
+        private readonly int _id = Guid.NewGuid().GetHashCode();
+        private int _dummyCount;
+
         private string SourceDirPath { get; } = Path.Combine(
             Path.GetDirectoryName(typeof(DummyFixture).Assembly.Location) ??
             Directory.GetCurrentDirectory()
         );
 
-        private string StorageDirPath => Path.Combine(SourceDirPath, "Dummies");
+        private string StorageDirPath => Path.Combine(SourceDirPath, $"Dummies_{_id}");
 
         private string CreatePortableExecutable(string sourceFileName)
         {
             var sourceFilePath = Path.Combine(SourceDirPath, sourceFileName);
 
             Directory.CreateDirectory(StorageDirPath);
-            var filePath = Path.Combine(StorageDirPath, $"PE-{Guid.NewGuid().GetHashCode()}.exe");
+
+            var fileIndex = Interlocked.Increment(ref _dummyCount);
+            var filePath = Path.Combine(StorageDirPath, $"PE-{fileIndex}.exe");
+
             File.Copy(sourceFilePath, filePath);
 
             return filePath;
