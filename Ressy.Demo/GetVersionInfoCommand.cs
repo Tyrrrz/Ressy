5c54c08fb2870f042e8b2679aefa9ab2fd996deb
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2023-02-20 03:59:25 +0200

    Replace Newtonsoft.Json with STJ

--- Ressy.Demo/GetVersionInfoCommand.cs
+++ Ressy.Demo/GetVersionInfoCommand.cs
@@ -1,9 +1,10 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using CliFx;
 using CliFx.Attributes;
 using CliFx.Infrastructure;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
+using Ressy.Demo.Utils;
 using Ressy.HighLevel.Versions;
 
 namespace Ressy.Demo;
@@ -11,12 +12,6 @@ namespace Ressy.Demo;
 [Command("read version", Description = "Read the version info resource from a PE file.")]
 public class GetVersionInfoCommand : ICommand
 {
-    private static readonly JsonSerializerSettings JsonSerializerSettings = new()
-    {
-        Formatting = Formatting.Indented,
-        Converters = { new StringEnumConverter() }
-    };
-
     [CommandOption("file", 'f', Description = "PE file to read the version info resource from.")]
     public required string FilePath { get; init; }
 
@@ -25,7 +20,20 @@ public class GetVersionInfoCommand : ICommand
         var portableExecutable = new PortableExecutable(FilePath);
         var versionInfo = portableExecutable.GetVersionInfo();
 
-        console.Output.WriteLine(JsonConvert.SerializeObject(versionInfo, JsonSerializerSettings));
+        console.Output.WriteLine(
+            JsonSerializer.Serialize(
+                versionInfo,
+                new JsonSerializerOptions
+                {
+                    WriteIndented = true,
+                    Converters =
+                    {
+                        new JsonStringEnumConverter(),
+                        new VersionAttributesJsonConverter()
+                    }
+                }
+            )
+        );
 
         return default;
     }
---
+++ Ressy.Demo/Utils/VersionAttributesJsonConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Ressy.HighLevel.Versions;
+
+namespace Ressy.Demo.Utils;
+
+// Dictionaries with non-string keys require a custom converter
+internal class VersionAttributesJsonConverter : JsonConverter<IReadOnlyDictionary<VersionAttributeName, string>>
+{
+    public override IReadOnlyDictionary<VersionAttributeName, string> Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options) =>
+        throw new NotSupportedException();
+
+    public override void Write(
+        Utf8JsonWriter writer,
+        IReadOnlyDictionary<VersionAttributeName, string> obj,
+        JsonSerializerOptions options)
+    {
+        writer.WriteStartObject();
+
+        foreach (var (key, value) in obj)
+            writer.WriteString(key, value);
+
+        writer.WriteEndObject();
+    }
+}
